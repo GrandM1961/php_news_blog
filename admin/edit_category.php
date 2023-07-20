18f0f4ec96c158d772a2cbc51757d26bc8114c5b
GrandM1961 <Hans@schootenvan.nl>
2023-07-20 12:38:35 +0200

    Add files via upload
    This one is also in Dutch and I altered the header into a echo header script.

--- admin/edit_category.php
+++ admin/edit_category.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
     include "parts/header.php";
     if(!isset($_POST['update']) && (empty($_GET['cid']) || empty($_GET['cname']) || !is_numeric($_GET['cid']))){
-        header("Location: category.php");
+        echo("<script>location.href = 'category.php';</script>");
     }
     if(isset($_POST['update']) && !empty($_POST['cname']) && !empty($_POST['cid'])){
         $cname = mysqli_real_escape_string($conn,$_POST['cname']);
@@ -10,24 +10,24 @@
         $ans = mysqli_query($conn,$sql);
         if($err = mysqli_error($conn)){die($err);}
         if($ans){
-            header("Location: category.php");
+            echo("<script>location.href = 'category.php';</script>");
         }else{
             echo "<div class='alert alert-danger'>fail!</div>";
         }
-        die("something is wrong");
+        die("sEr ging iets fout.");
     }
     $cid = mysqli_real_escape_string($conn,$_GET['cid']);
 ?>
-        <h2 class="text-capitalize p-3">edit category</h2>
+        <h2 class="text-capitalize p-3">Categorie Bewerken</h2>
         <form autocomplete="off" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="w-50 m-auto bg-light p-3 text-capitalize" method="post">
             <div class="mb-3 row">
-                <label class="col-sm-2 col-form-label">category name</label>
+                <label class="col-sm-2 col-form-label">Categorienaam</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" name="cname" value="<?php echo $_GET['cname']; ?>" required>
                     <input type="hidden" name="cid" value="<?php echo $_GET['cid']; ?>">
                 </div>
             </div>
-            <input type="submit" class="btn btn-dark d-block" value="update" name="update">
+            <input type="submit" class="btn btn-dark d-block" value="Bijwerken" name="update">
         </form>
     </div>
 </body>
