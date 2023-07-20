4b1ae5ce47e5059c3a4092f327259f88366bf5e1
GrandM1961 <Hans@schootenvan.nl>
2023-07-20 12:32:53 +0200

    Add files via upload
    This is a altered file from edit_post.php that won't show the videolink when a videolink is empty

--- admin/edit_post.php
+++ admin/edit_post.php
@@ -1,8 +1,8 @@
-<?php 
-    include "parts/header.php";
+<?php
+    include "includes/head.php";
     $alert = "";
     if(!isset($_POST['update']) && (empty($_GET['pid']) || !is_numeric($_GET['pid']))){
-        header("Location: post.php");
+        echo("<script>location.href = 'post.php';</script>");
     }
     function process($newimage){
         global $conn;
@@ -19,7 +19,7 @@
         $ans = mysqli_multi_query($conn,$sql);
         if($err = mysqli_error($conn)){die($err);}
         if($ans){
-            header("Location: post.php");
+            echo("<script>location.href = 'post.php';</script>");
         }else{
             $alert = "<div class='alert alert-danger'>something happen wrong. </div>";
         }
@@ -68,64 +68,116 @@
             $cat = $row1['pcat'];
             $img = $row1['pimage'];
         }else{
-            header("Location: post.php");
+            echo("<script>location.href = 'post.php';</script>");
         }
-    }
+      }
+      include "includes/header.php";
 ?>
-        <h2 class="text-capitalize p-3">edit post</h2>
-        <div class="row justify-content-center">
-            <div class="col-6 text-center"><?php echo $alert; ?></div>
-        </div>
-        <form autocomplete="off" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="w-50 m-auto bg-light p-3 text-capitalize" method="post" enctype="multipart/form-data">
-            <div class="mb-3 row">
-                <label class="col-sm-2 col-form-label">title</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" name="title" value="<?php echo $title; ?>" required>
-                    <input type="hidden" name="pid" value="<?php echo $pid; ?>">
-                    <input type="hidden" name="old_image" value="<?php echo $img; ?>">
-                    <input type="hidden" name="old_cat" value="<?php echo $cat; ?>">
+          <!-- end: header -->
+          <div class="inner-wrapper">
+            <!-- start: sidebar -->
+            <?php include('includes/sidebarlinks.php'); ?>
+            <!-- end: sidebar -->
+            <section role="main" class="content-body">
+              <header class="page-header">
+                <h2>Berichten Overzicht</h2>
+                <div class="right-wrapper text-end">
+                  <ol class="breadcrumbs">
+                    <li>
+                      <a href="dashboard.php"><i class="bx bx-home-alt"></i></a>
+                    </li>
+                    <li><span>Nieuws</span></li>
+                    <li><a href="post.php"></a><span>Berichten Overzicht</span></li>
+                  </ol>
+                  <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fas fa-chevron-left"></i></a>
                 </div>
-            </div>
-            <div class="mb-3 row">
-                <label class="col-sm-2 col-form-label">description</label>
-                <div class="col-sm-10">
-                    <textarea name="desc" class="form-control" cols="30" rows="10" required><?php echo $desc; ?></textarea>
+              </header>
+              <!-- start: page -->
+
+                <div class="row">
+                  <div class="col">
+                    <section class="card">
+                      <header class="card-header">
+                        <div class="card-actions">
+                          <a href="#" class="card-action card-action-toggle" data-card-toggle></a>
+                        </div>
+                        <h2 class="card-title text-capitalize p-3">Alle berichten</h2>
+                        <div class="col-md-3"><a href="add_post.php" class="btn btn-default">Bericht Toevoegen</a></div>
+                      </header>
+                      <div class="card-body">
+                          <div class="row justify-content-center">
+                              <div class="col-6 text-center"><?php echo $alert; ?></div>
+                          </div>
+                          <form autocomplete="off" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="w-50 m-auto bg-light p-3 text-capitalize" method="post" enctype="multipart/form-data">
+                            <div class="mb-3 row">
+                                <label class="col-sm-2 col-form-label">Post-ID</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="pid" value="<?php echo $pid; ?>">
+                                    <input type="hidden" name="pid" value="<?php echo $pid; ?>">
+                                </div>
+                            </div>
+                              <div class="mb-3 row">
+                                  <label class="col-sm-2 col-form-label">Titel</label>
+                                  <div class="col-sm-10">
+                                      <input type="text" class="form-control" name="title" value="<?php echo $title; ?>" required>
+                                      <input type="hidden" name="pid" value="<?php echo $pid; ?>">
+                                      <input type="hidden" name="old_image" value="<?php echo $img; ?>">
+                                      <input type="hidden" name="old_cat" value="<?php echo $cat; ?>">
+                                  </div>
+                              </div>
+                              <div class="mb-3 row">
+                                  <label class="col-sm-2 col-form-label">Beschrijving</label>
+                                  <div class="col-sm-10">
+                                      <textarea name="desc" id="desc" class="form-control" cols="30" rows="10" ><?php echo $desc; ?></textarea>
+                                  </div>
+                              </div>
+                               <div class="mb-3 row">
+                                  <label class="col-sm-2 col-form-label">Miniatuur</label>
+                                  <div class="col-sm-10">
+                                      <input type="file" class="form-control" name="img">
+                                  </div>
+                              </div>
+                              <?php if(empty($videolink)!=""){
+                                echo '<div class="mb-3 row">
+                                    <label class="col-sm-2 col-form-label">Oude Afbeelding</label>
+                                    <div class="col-sm-10">
+                                        <img src="../images/' .$img.'" class="img-thumbnail w-50">
+                                    </div>
+                                </div>';
+                              }?>
+
+                              <div class="mb-3 row">
+                                    <label class="col-sm-2 col-form-label">Categorie</label>
+                                    <div class="col-sm-10">
+                                        <select class="form-select" name="category" required>
+                                              <?php
+                                                $query = mysqli_query($conn,"SELECT * FROM category");
+                                                if($err = mysqli_error($conn)){die($err);}
+                                                if(mysqli_num_rows($query) > 0){
+                                                    echo "<option selected disabled>Selecteer Categorie</option>";
+                                                    while($row = mysqli_fetch_assoc($query)){
+                                                        $se = ($cat == $row['cid'])? "selected":"";
+                                                        echo "<option $se value='{$row['cid']}'>{$row['cname']}</option>";
+                                                    }
+                                                }else{
+                                                    echo "<option selected disabled>Geen Categorie gevonden.</option>";
+                                                }
+                                              ?>
+                                        </select>
+                                    </div>
+                                </div>
+                              <input type="submit" class="btn btn-dark d-block" value="Bijwerken" name="update">
+                          </form>
+                      </div>
+                    </section>
+                  </div>
                 </div>
+                <!-- end: page -->
+              </section>
             </div>
-            <div class="mb-3 row">
-                <label class="col-sm-2 col-form-label">thumbnail</label>
-                <div class="col-sm-10">
-                    <input type="file" class="form-control" name="img">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label class="col-sm-2 col-form-label">old image</label>
-                <div class="col-sm-10">
-                    <img src="../images/<?php echo $img; ?>" class="img-thumbnail w-50">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label class="col-sm-2 col-form-label">category</label>
-                <div class="col-sm-10">
-                    <select class="form-select" name="category" required>
-                          <?php
-                            $query = mysqli_query($conn,"SELECT * FROM category");
-                            if($err = mysqli_error($conn)){die($err);}
-                            if(mysqli_num_rows($query) > 0){
-                                echo "<option selected disabled>select category.</option>";
-                                while($row = mysqli_fetch_assoc($query)){
-                                    $se = ($cat == $row['cid'])? "selected":"";
-                                    echo "<option $se value='{$row['cid']}'>{$row['cname']}</option>";
-                                }
-                            }else{
-                                echo "<option selected disabled>no category found.</option>";
-                            }
-                          ?> 
-                    </select>
-                </div>
-            </div>
-            <input type="submit" class="btn btn-dark d-block" value="update" name="update">
-        </form>
-    </div>
-</body>
-</html>
+            <?php include('includes/sidebarrechts.php'); ?>
+            </section>
+            <?php include('includes/scripts.php'); ?>
+            <!-- Examples -->
+          </body>
+      </html>
